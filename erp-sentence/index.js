159f9cbf90b7fdad172653ae0e503532fcddbecd
lingxiu <47123365+rocketcrane@users.noreply.github.com>
2022-10-11 19:12:21 -0400

    modified timeout

--- erp-sentence/index.js
+++ erp-sentence/index.js
@@ -10,7 +10,7 @@ var JsPsychERPSentence = (function (jspsych) {
       },
       delay: {
         type: jspsych.ParameterType.INT,
-        default: 500,
+        default: 1000,
       },
       index: {
         type: jspsych.ParameterType.INT,
@@ -35,28 +35,34 @@ var JsPsychERPSentence = (function (jspsych) {
     }
     trial(display_element, trial) {
 		var wordArray = trial.stimulus.split(/\s/); //splits sentence by space character into an array of words
-		console.log(wordArray); //debug
 		var word = '';
+		display_element.innerHTML = trial.stimulus;
+		console.log("now");
+		
 		
 		/* display all words one-by-one */
 		for (let i = 0; i < wordArray.length; i++) {
-			word = '<div>' + wordArray[i] + '</div>';
-			console.log(word); //debug
-			display_element.innerHTML = word;
+			var num = i + 1;
+			var time = num * trial.delay;
 			
-			/* set event marker at oddball */
-			if (i + 1 == trial.index) {
-				//wip
+			function displayWord(i) {
+				word = '<div>' + wordArray[i] + '</div>';
+				display_element.innerHTML = word;
+				
+				/* set event marker at oddball */
+				if (num == trial.index) {
+					console.log("oddball");
+				}
 			}
 			
 			/* wait for duration specified by delay parameter */
-			this.jsPsych.pluginAPI.setTimeout(()=>{}, trial.delay);
+			this.jsPsych.pluginAPI.setTimeout(displayWord(i), time);
 		}
-		
-		//display_element.innerHTML = ''; //clear display
-		
+				
     	/* end trial */
-    	this.jsPsych.finishTrial();
+		this.jsPsych.pluginAPI.setTimeout(function(){
+			this.jsPsych.finishTrial();
+		}, trial.delay * (wordArray.length + 1));
     }
   }
   ERPSentencePlugin.info = info;
