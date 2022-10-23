8b730a83e7fc251d50433cf612f2ce93d538a0fc
lingxiu <47123365+rocketcrane@users.noreply.github.com>
2022-10-23 17:49:44 -0400

    single trials functional

--- erp-sentence/index.js
+++ erp-sentence/index.js
@@ -1,86 +1,84 @@
 var JsPsychERPSentence = (function (jspsych) {
-  "use strict";
+	  "use strict";
 
-  const info = {
-    name: "erp-sentence",
-    parameters: {
-      stimulus: {
-        type: jspsych.ParameterType.HTML_STRING,
-        default: undefined,
-      },
-      delay: {
-        type: jspsych.ParameterType.INT,
-        default: 1000,
-      },
-      index: {
-        type: jspsych.ParameterType.INT,
-        default: undefined,
-      },
-    },
-  };
+	const info = {
+		name: "erp-sentence",
+		parameters: {
+			stimulus: {
+		    	type: jspsych.ParameterType.HTML_STRING,
+		    	default: undefined,
+		  	},
+		  	delay: {
+		    	type: jspsych.ParameterType.INT,
+		    	default: 1000,
+		  	},
+		  	index: { //index begins at 1, not 0
+		  	  	type: jspsych.ParameterType.INT,
+		   	 	default: undefined,
+		  	},
+		},
+	};
 
-  /**
-   * **ERP-SENTENCE**
-   *
-   * Displays sentences word by word.
-   * Sends event marker at oddball location, and gathers keyboard response at the end.
-   * For ERP experiments involving N400/P600 syntatic/semantic oddballs.
-   *
-   * @author Lingxiu Zhang @rocketcrane
-   * @see {documentation in progress}
-   */
-  class ERPSentencePlugin {
-    constructor(jsPsych) {
-      this.jsPsych = jsPsych;
-    }
-    trial(display_element, trial) {
-		var wordArray = trial.stimulus.split(/\s/); //splits sentence by space character into an array of words
-		var word = '';
-		var data;
-		
-		/* display all words one-by-one */
-		for (let i = 0; i < wordArray.length; i++) {
-			var num = i + 1;
-			var time = num * trial.delay;
-			
-			function displayWord() {
-				word = '<div>' + wordArray[i] + '</div>';
-				display_element.innerHTML = word;
-				
-				/* set event marker at oddball */
-				if (num == trial.index) {
-					console.log("oddball");
-				}
-			}
-			/* wait for duration specified by delay parameter */
-			this.jsPsych.pluginAPI.setTimeout(function() {displayWord()}, time);
+	/**
+	* **ERP-SENTENCE**
+	*
+	* Displays sentences word by word.
+	* Sends event marker at oddball location, and gathers keyboard response at the end.
+	* For ERP experiments involving N400/P600 syntatic/semantic oddballs.
+	*
+	* @author Lingxiu Zhang @rocketcrane
+	* @see {documentation in progress}
+	*/
+	class ERPSentencePlugin {
+		constructor(jsPsych) {
+		  this.jsPsych = jsPsych;
 		}
-		
-		/* display */
-		
-    	/* keyboard response & end trial */
-		this.jsPsych.pluginAPI.setTimeout(function(){
-			const after_key_response = (info) => {
-			    /* record the response time as data */
-			    let data = {
-			      rt: info.rt
-			    }
-			}
-		    /* set up a keyboard event to respond */
-		    this.jsPsych.pluginAPI.getKeyboardResponse({
-		      callback_function: after_key_response,
-		      valid_responses: ['f', 'j'],
-		      persist: false
-		    });
-		}, trial.delay * trial.index);
-		
-	    /* end trial */
-		this.jsPsych.pluginAPI.setTimeout(function(){
-	    	this.jsPsych.finishTrial(data);
-		}, trial.delay * wordArray.length);
-    }
-  }
-  ERPSentencePlugin.info = info;
+		trial(display_element, trial) {
+			var wordArray = trial.stimulus.split(/\s/); //splits sentence by space character into an array of words
+			var word = '';
+		    var data = {
+		    	rt: -99999
+		    }
 
-  return ERPSentencePlugin;
+			/* display all words one-by-one */
+			for (let i = 0; i < wordArray.length; i++) {
+								
+				/* wrap word display in a function to run using timeout */
+				function displayWord() {
+					word = '<div>' + wordArray[i] + '</div>'; //html formatting
+					display_element.innerHTML = word; //display the word
+		
+					/* trigger handler at the oddball word */
+					if ((i + 1) == trial.index) {
+						//WORK IN PROGRESS: fetch(`http://127.0.0.1:8000/trigger/tcp/${jsPsych.timelineVariable("trigger_value")}`);
+						console.log("trigger");
+					}
+				}
+				/* wait for duration specified by delay parameter */
+				this.jsPsych.pluginAPI.setTimeout(function() {displayWord()}, (i + 1) * trial.delay);
+			}
+			
+			/* keyboard response & end trial */
+			this.jsPsych.pluginAPI.setTimeout(function(){
+				const after_key_response = (info) => {
+				    /* record the response time */
+				    data.rt = info.rt;
+				}
+			    /* set up a keyboard event to respond */
+			    this.jsPsych.pluginAPI.getKeyboardResponse({
+			    	callback_function: after_key_response,
+			    	valid_responses: ['f', 'j'], //WORK IN PROGRESS: only F and J responses accepted for now
+			    	persist: false
+			    });
+			}, trial.delay * trial.index);
+		
+		    /* end trial */
+			this.jsPsych.pluginAPI.setTimeout(function(){
+		    	this.jsPsych.finishTrial(data);
+			}, trial.delay * (wordArray.length + 1)); //end trial only after last word has been displayed for time specified by delay variable
+		}
+	}
+	ERPSentencePlugin.info = info;
+
+	return ERPSentencePlugin;
 })(jsPsychModule);
